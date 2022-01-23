50f35dfa8a6b04f7cb592d2f5c948945446e397d
ArtOfWar <camocrocsnft@gmail.com>
2022-01-22 18:02:05 -0800

    Prechecker to do sanity checks

--- camocrocs_engine.py
+++ camocrocs_engine.py
@@ -2,12 +2,21 @@ from configparser import Config
 from meta_writer import MetaWriter
 from trait_generator import TraitGenerator
 from image_generator import ImageGenerator
+from prechecker import Prechecker
 from validator import Validator
 from os.path import abspath
 
 if __name__ == '__main__':
     print('Parsing config...')
     config = Config()
+
+    print('Doing a sanity check on your inputs...')
+    prechecker = Prechecker(config)
+    ret = prechecker.validate()
+    if not ret:
+        print('Your input has problems. Check the output above and fix them first.')
+        exit(1)
+
     print('\nGenerating unique traits...')
     traitgen = TraitGenerator(config)
     try:
---
+++ prechecker.py
@@ -0,0 +1,46 @@
+import os
+from os.path import join
+from pathlib import Path
+import json
+from configparser import FORMAT_SOLANA
+from PIL import Image
+
+ok = '\u2713'
+notok = '\u2717'
+
+
+class Prechecker:
+    def __init__(self, config):
+        self.config = config
+
+    def validate(self):
+        ret = True
+        total = self.config.total_images
+        width = self.config.image_options.width
+        height = self.config.image_options.height
+        # Check if all images are png and of correct resolution
+        # {Layer: [Image]}
+        print('Checking base layers')
+        for layer, images in self.config.layer_images_map.items():
+            for im in images:
+                im = Image.open(im.filename)
+                if im.width != width or im.height != height:
+                    print(f'{notok} Expected {im.filename} to be of resolution {width}x{height} but it is {im.width}x{im.height}')
+                    ret = ret and False
+                if im.format != 'PNG':
+                    print(f'{notok} Expected {im.filename} to be a PNG but it is {im.format}')
+                    ret = ret and False
+
+        if ret:
+            print(f'{ok} All checks passed')
+        else:
+            print(f'{notok} Some checks failed')
+        return ret
+
+
+if __name__ == '__main__':
+    from configparser import Config
+
+    c = Config()
+    v = Prechecker(c)
+    v.validate()
